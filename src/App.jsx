599fbd064e942451c664d9610c66d302119bd887
Samuel-Omobuwa <samuelomobuwa1@gmail.com>
2025-03-24 03:23:12 +0100

    input logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 const initialItems = [
   { id: 1, description: "Passports", quantity: 2, packed: false },
   { id: 2, description: "Socks", quantity: 12, packed: true },
@@ -18,16 +20,38 @@ function Logo() {
   return <h1>🌴Far Away 💼</h1>;
 }
 function Form() {
+  const [description, setDescription] = useState("");
+  const [quantity, setQuantity] = useState(3);
+
+  function handleItemSubmit(e) {
+    e.preventDefault();
+
+    const newItem = { description, quantity, packed: false, id: Date.now() };
+    initialItems.push(newItem);
+    console.log(initialItems);
+  }
+
   return (
-    <div className="add-form">
-      <select>
+    <form className="add-form" onSubmit={handleItemSubmit}>
+      <select
+        value={quantity}
+        onChange={(e) => setQuantity(Number(e.target.value))}
+      >
         {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
           <option value={num} key={num}>
             {num}
           </option>
         ))}
       </select>
-    </div>
+
+      <input
+        type="text"
+        placeholder="Item..."
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+      />
+      <button onClick={() => initialItems.push(newItem)}>Add</button>
+    </form>
   );
 }
 function PackingList() {
